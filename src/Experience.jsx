14700c44932ec1bf8777f96cfc9a70dfe30993f5
Pavankalyan Nayak Guguloth <97593632+pavannaiik@users.noreply.github.com>
2025-04-28 22:41:55 +0000

    Updated Experience.jsx with revised experience details and layout

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -26,8 +26,8 @@ export default function Experience() {
             </div>
             <div className="flex justify-center items-center">
               <Fade bottom duration={1200}>
-                <div style={{ width: "300px", height: "250px" }} className="center">
-                  <Lottie animationData={ExperienceAnimation}></Lottie>
+                <div className="lottie-container">
+                  <Lottie animationData={ExperienceAnimation} />
                 </div>
               </Fade>
             </div>
@@ -35,17 +35,15 @@ export default function Experience() {
           <ExperienceCard
             logo={MacmillanLogo}
             company="Macmillan"
-            title="Software Developer Intern"
+            title="Software Developer"
             duration="June 2024 — August 2024"
             location="New York, USA"
             description={[
-              "Developed and automated a workflow to publish solution articles from Google Docs to Freshdesk, reducing manual processing\
-              time by 60% using Python and Django by integrating Freshdesk API and Google Doc API, improving content management\
-              by 80%", "Integrated Google Gemini model into the product for publishing the Knowledge Based tickets from Asana portal to Freshdesk\
-              chat Help section using Python and AI/ML which boosted user engagement by 30%",
-              "Participated in an intern project addressing current publishing-related challenges."
+              "Automated a workflow to publish solution articles from Google Docs to Freshdesk using Spring Boot and AWS S3, reducing manual processing time by 95% and improving content management workflow.",
+              "Integrated OpenAI model into the product for publishing the Knowledge Base tickets from Asana portal to Freshdesk chat Help section using Java which boosted user engagement by 30%.",
+              "Led a team of four to deliver an intern project in the domain Future of AI for the Book Recommender System combining content-based filtering and collaborative filtering to provide recommendations with average number of clicks per user at 73%."
             ]}
-            techStack={["Python", "Django", "Flask", "Google Gemini","Postgres"]}
+            techStack={["Java", "SpringBoot", "PostgreSQL", "OpenAI", "Google Docs"]}
           />
           <ExperienceCard
             logo={OracleLogo}
@@ -54,9 +52,10 @@ export default function Experience() {
             duration="May 2022 - August 2023"
             location="Hyderabad, India"
             description={[
-              "Led complete SDLC of HCM Celebrate product, improving data consistency to 100%.",
-              "Implemented Under Budget Restriction functionality, improving budget adherence by 80%.",
-              "Developed automation frameworks, reducing regression testing time by 70%."
+              "Worked on complete software development lifecycle (SDLC) of HCM Celebrate product right from requirements gathering, coding, code review, testing, deploying, bug fixing, and code/API documentation.",
+              "Developed PLSQL scripts for distributing reward points to employees on a monthly basis and worked on an Explicit Locking Mechanism (Oracle) for concurrent updates of likes and comments by users.",
+              "Optimized SQL queries and database performance in Oracle, achieving a 30% improvement in application performance.",
+              "Resolved 95% of production issues swiftly, enhancing product reliability and customer satisfaction."
             ]}
             techStack={["Java", "Oracle DB", "Rest API", "VBCS", "Git", "Kubernetes", "PL/SQL"]}
           />
@@ -67,9 +66,11 @@ export default function Experience() {
             duration="September 2020 - May 2022"
             location="Bengaluru, India"
             description={[
-              "Built and maintained RESTful services for 'Medway' application, improving system performance.",
-              "Enhanced SQL query performance by 75% through collaboration with DBAs.",
-              "Implemented Kafka for real-time logging, improving system monitoring by 90%."
+              "Built high-performance, secure, resilient services with clean, well-documented RESTful APIs for the ’Medway’ application based on micro-services architecture in Java using SpringBoot framework containerized with Docker.",
+              "Utilizing Apache Kafka, developed a data feed solution that feeds into a dashboard, providing management with daily oversight to track order volumes and processing times.",
+              "Implemented automated deployment triggers to staging, streamlining the CI/CD pipeline process, which increased deployment frequency to staging by 40% allowing frequent testing and validation in the staging environment.",
+              "Designed relational database schemas and optimized SQL queries for faster data retrieval.",
+              "Achieved code coverage of 90% in a TDD environment by writing unit tests using JUNIT5, Mockito."
             ]}
             techStack={["Java", "SpringBoot", "PostgreSQL", "AWS", "RabbitMQ", "R"]}
           />
@@ -80,7 +81,7 @@ export default function Experience() {
             duration={800}
           >
             <div className="absolute left-1/2 transform -translate-x-1/2 mb-8 pt-10">
-              <Lottie animationData={DownAnimation}></Lottie>
+              <Lottie animationData={DownAnimation} />
             </div>
           </Link>
         </div>
@@ -101,7 +102,7 @@ const ExperienceCard = ({ logo, company, title, duration, location, description,
         </h4>
         <p className="mt-6 text-base leading-7 text-white">
           {description.map((desc, index) => (
-            <span key={index}>
+            <span key={`desc-${index}`}>
               {desc}
               <br />
             </span>
@@ -115,7 +116,7 @@ const ExperienceCard = ({ logo, company, title, duration, location, description,
         </div>
         <ul role="list" className="mt-8 grid grid-cols-3 gap-6 text-sm leading-6 text-white sm:grid-cols-3 sm:gap-6">
           {techStack.map((tech, index) => (
-            <li key={index} className="flex flex-col items-center gap-3 sm:flex-row sm:items-center sm:justify-start">
+            <li key={`tech-${index}`} className="flex flex-col items-center gap-3 sm:flex-row sm:items-center sm:justify-start">
               <span className="relative z-10 rounded-full bg-white px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-400">
                 {tech}
               </span>
@@ -126,7 +127,7 @@ const ExperienceCard = ({ logo, company, title, duration, location, description,
       <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
         <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16 h-full">
           <div className="mx-auto max-w-xs h-full px-8 flex justify-center items-center">
-            <img src={logo} alt={`${company} logo`} className="w-full h-auto" />
+            <img src={logo} alt={`Logo of ${company}`} className="w-full h-auto" />
           </div>
         </div>
       </div>
